d1a407f8fe7a0d0fbe1b28ee037313b07ae4d62f
gigi-lopa <gilbert2klopah@gmail.com>
2026-05-29 15:25:49 +0200

    feat: setup backend

---
+++ frontend/src/components/dialogues/ViewReportDialogue.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../ui/button";
+import { Dialog, DialogTrigger } from "../ui/dialog";
+
+function ViewReportDialogue() {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button>View Report </Button>
+      </DialogTrigger>
+    </Dialog>
+  )
+}
+
+export default ViewReportDialogue
