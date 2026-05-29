fe0f3c54460e2bb8d89715f584901decad8ffff5
gigi-lopa <gilbert2klopah@gmail.com>
2026-05-29 15:50:50 +0200

    feat: setup user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,21 @@
+from apps.common.user_manager import UserManager   
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = UserManager()    
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=["first_name", "last_name", "email"]
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}: {'Client' if not self.is_staff else 'Admin'}"
