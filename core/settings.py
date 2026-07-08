b3d594590397bf3e783c1cdb6b7a3b12025eacb7
gigi-lopa <gilbert2klopah@gmail.com>
2026-07-08 16:27:40 +0200

    update:cors

--- core/settings.py
+++ core/settings.py
@@ -96,7 +96,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
-    "https://fincheck-biu-reports.onrender.com"
+    "https://fincheck-biu-reports.onrender.com",
+    "https://biu-reports.vercel.app/"
 ]
 
 CORS_ALLOW_METHODS = [
@@ -112,6 +113,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://fincheck-biu-reports.onrender.com",
+    "https://biu-reports.vercel.app/"
 ]
 
 # Database
